2306241da032263a3d567ac600e44a8e026622de
pc <pc@LAPTOP-5AKPLK9U>
2022-03-11 11:24:20 +0700

    Add project files.

---
+++ maytinh_2/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace maytinh_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += "0";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += "1";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += "2";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += "3";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += "4";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += "5";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += "6";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += "7";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += "8";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += "9";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += ".";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += ",/,";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += ",*,";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += ",-,";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            PTtext.Text += ",+,";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
